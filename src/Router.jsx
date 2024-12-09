8f04e1de36173eed1fd5df5ff489a1128ba9bd8c
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-12-09 07:27:52 +0300

    vercel and reactlazy adde

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router";
-import Home from "./pages/HomePage";
-import Movies from "./pages/MoviesPage";
 import Layout from "./components/Layout";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
-import NotFoundPage from "./pages/NotFoundPage";
+
+const Home = lazy(() => import("./pages/HomePage"));
+const Movies = lazy(() => import("./pages/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
 export default function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetailsPage />} />
-        <Route path="/movies/:movieId/cast" element={<MovieDetailsPage />} />
-        <Route path="/movies/:movieId/reviews" element={<MovieDetailsPage />} />
-        <Route path="*" element={<NotFoundPage />}/>
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>loading the page...</div>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />} />
+          <Route path="/movies/:movieId/cast" element={<MovieDetailsPage />} />
+          <Route
+            path="/movies/:movieId/reviews"
+            element={<MovieDetailsPage />}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
